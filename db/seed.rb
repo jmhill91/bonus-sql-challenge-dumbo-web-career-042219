390b3e2c1a549aadb0276277c1af3e6a7ac50d17
github username <github email address>
2019-05-02 18:28:52 -0400

    Done.

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,11 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+
+
+require 'csv' path = "../daily_show_guests.csv"
+csv_data = CSV.read(path)
+
+csv_data.parse(csv, headers: true) do |row|
+  db.execute "insert into users values ( ?, ? )", row.fields # equivalent to: [row['name'], row['age']]
+end
+
+db.execute( "select * from users" )
